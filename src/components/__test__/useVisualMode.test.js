c775741d327da083b7b45783a320e7a20b320729
Soyoung <ksyoung3424@gmail.com>
2023-09-06 03:33:05 +0000

    Adding Transition

--- src/components/__test__/useVisualMode.test.js
+++ src/components/__test__/useVisualMode.test.js
@@ -3,6 +3,7 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
+const SECOND = "SECOND";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -11,4 +12,11 @@ test("useVisualMode should initialize with default value", () => {
   // The result of rendering the hook is stored in the result variable
 
   expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 });
